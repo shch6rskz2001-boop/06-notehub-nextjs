01f46d7a31f31d659c5f0a1f14fee0397792b208
shch6rskz2001-boop <shch6rskz2001@gmail.com>
2026-05-26 12:35:11 +0300

    fleks

--- app/notes/[id]/error.tsx
+++ app/notes/[id]/error.tsx
@@ -2,12 +2,12 @@
 
 interface Props {
   error: Error & { digest?: string };
-  reset: () => void;
 }
 
 export default function NoteError({ error }: Props) {
   return <p>Could not fetch note details. {error.message}</p>;
 }
+
 
 
 
